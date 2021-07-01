ee387d66b5c9e7e5967e15461049c22304780049
kimjieon0 <75726666+kimjieon0@users.noreply.github.com>
2021-07-01 20:32:25 +0900

    Create receiptArrayList.java

---
+++ receiptArrayList.java
@@ -0,0 +1,30 @@
+package receipt;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class receiptArrayList {
+   
+   int no;
+   int price;
+   int count;
+   String name;
+   String text;
+   int total;
+   
+   /*ArrayList<Integer> ArrNo = new ArrayList<Integer>();
+   ArrayList<String> ArrMenu = new ArrayList<String>();
+   ArrayList<Integer> ArrPrice = new ArrayList<Integer>();
+   ArrayList<String> ArrText = new ArrayList<String>();
+   
+   public static void AddOrder() {
+      Scanner myInput = new Scanner(System.in);
+
+      
+      ArrNo.add(no);
+      ArrMenu.add(menu);
+      ArrPrice.add(price);
+      ArrText.add(text);
+   }*/
+
+}
+
